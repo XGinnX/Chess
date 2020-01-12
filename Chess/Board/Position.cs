1830110819341b118bb19c9414dec59a0ce209cb
Jean Sales Santos <37668112+XGinnX@users.noreply.github.com>
2020-01-12 13:37:54 -0300

    Add files via upload
    Board Creation.

---
+++ Chess/Board/Position.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Board
+{
+    class Position
+    {
+        public int Row { get; set; }
+        public int Collumn { get; set; }
+
+        public Position(int line, int collumn)
+        {
+            this.Row = line;
+            this.Collumn = collumn;
+        }
+        public override string ToString()
+        {
+            return Row +" ,"+Collumn;
+        }
+    }
+}
---
+++ Chess/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Board;
+
+namespace Chess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Position P;
+            P = new Position(3,4);
+            Console.WriteLine(P);
+        }
+    }
+}
